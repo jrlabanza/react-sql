9dfc323ec786ac554b0b2b089c6c4ef4d55329b7
jrlabanza <jrlabanza@gmail.com>
2020-05-29 17:19:57 +0800

    git push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    products: [],
+    product: {
+      handlerID: 'sample product',
+      testerID:20
+    }
+  }
+
+  componentDidMount(){
+    this.getProducts();
+  }
+
+  getProducts = _ => {
+    fetch('http://localhost:3000/products')
+    .then(response => response.json())
+    .then(response => this.setState({ products: response.data}))
+    .catch(err=> console.error(err))
+  }
+
+  addProduct = _ => {
+
+  }
+
+renderProduct = ({ id, handlerID, testerID, description, remarks}) => <div key={id}>{handlerID}{' '}{remarks}</div>
+
+  render(){
+    const { products } = this.state;
+    return (
+      <div className="App">
+        {products.map(this.renderProduct)}
+
+        <div>
+          <input/>
+        </div>
+      </div>
+    );
+  }
 }
+//https://www.youtube.com/watch?v=HPIjjFGYSJ4 continue at home
 
 export default App;
